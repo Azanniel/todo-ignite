8f025c581b702caa186fa868b388be07374ea207
azanniel <leo.azannielttt@gmail.com>
2023-01-04 23:58:37 -0400

    💩 created form and input task

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,18 @@ export function App() {
   return (
     <div>
       <Header />
+
+      <main className={styles.container}>
+        <form className={styles.form}>
+          <input
+            className={styles.field}
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+          />
+
+          <button>Criar</button>
+        </form>
+      </main>
     </div>
   )
 }
