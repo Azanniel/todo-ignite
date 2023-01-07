7fcaa98d59d03049554b9e08b2da09e007397a31
azanniel <leo.azannielttt@gmail.com>
2023-01-06 23:26:56 -0400

    ✨ created empty component for tasks list

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { PlusCircle } from 'phosphor-react';
 import { Header } from './components/Header';
 import { Input } from './components/Input';
 
+import clipboard from './assets/clipboard.png';
+
 import styles from './App.module.css';
 
 export function App() {
@@ -17,12 +19,46 @@ export function App() {
             placeholder="Adicione uma nova tarefa"
           />
 
-          <button className={styles.submit}>
+          <button type="submit" className={styles.submit}>
             <span>Criar</span>
 
             <PlusCircle size={16} weight="bold" />
           </button>
         </form>
+
+        <div className={styles.tasks}>
+          <header className={styles.info}>
+            <div>
+              <strong className={styles.created}>
+                Tarefas criadas
+              </strong>
+
+              <span className={styles.counter}>0</span>
+            </div>
+
+            <div>
+              <strong className={styles.done}>
+                Concluídas
+              </strong>
+
+              <span className={styles.counter}>0</span>
+            </div>
+          </header>
+
+          <main>
+            <section className={styles.empty}>
+              <img
+                src={clipboard}
+                alt="bloco de anotações com linhas preenchidas e tons de cinza"
+              />
+
+              <p>
+                <strong>Você ainda não tem tarefas cadastradas</strong>
+                <span>Crie tarefas e organize seus itens a fazer</span>
+              </p>
+            </section>
+          </main>
+        </div>
       </main>
     </div>
   )
